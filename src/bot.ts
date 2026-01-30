c53af17773d3323de20b6d350eb223ecee0bb1bb
lilileiva <kaffidelili@gmail.com>
2026-01-29 23:14:30 -0300

    feat: Add startPoll, sendPhoto and  sendDocumentmethods to `Bot` class

--- src/bot.ts
+++ src/bot.ts
@@ -12,7 +12,7 @@ export class Bot {
   constructor(token: string, chatId: string) {
 
     this.chatId = chatId;
-    this.bot = new TelegramBot(token, { polling: false });
+    this.bot = new TelegramBot(token, { polling: true });
   }
 
   /**
@@ -27,6 +27,49 @@ export class Bot {
     );
     } catch (error) {
       console.error("Error sending message to bot: ", error)
+    }
+  }
+
+  /**
+   * Starts listening for poll answers.
+   * @param command The command to listen for.
+   * @param callback The callback function to handle poll answers.
+   */
+  startPoll(command: string, callback: (msg: any) => void) {
+    this.bot.on(command, callback);
+  }
+
+  /**
+   * Sends a photo to the bot.
+   * @param photo The photo to send
+   * @param caption The caption to send
+   */
+  async sendPhoto(photo: string | Buffer, caption?: string) {
+    try {
+      await this.bot.sendPhoto(
+        this.chatId,
+        photo,
+        { caption }
+    );
+    } catch (error) {
+      console.error("Error sending photo to bot: ", error)
+    }
+  }
+
+  /**
+   * Sends a document to the bot.
+   * @param document The document to send
+   * @param caption The caption to send
+   */
+  async sendDocument(document: string | Buffer, caption?: string) {
+    try {
+      await this.bot.sendDocument(
+        this.chatId,
+        document,
+        { caption }
+    );
+    } catch (error) {
+      console.error("Error sending document to bot: ", error)
     }
   }
 }
